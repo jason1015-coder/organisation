af734fdd141cb716647da21a9d78b6dfb826cc41
Will Lamerton <william.lamerton@gmail.com>
2026-07-05 21:45:26 +0100

    feat: add prompt-scrub to footer products list
    Insert prompt-scrub in the Products column of the Footer between get-md and json-up, linking to /prompt-scrub.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -57,6 +57,17 @@ export function Footer() {
                   </Link>
                 </li>
                 <li>
+                  <Link
+                    href="/prompt-scrub"
+                    className="hover:text-[#0000EE] dark:hover:text-[#A1A1AA] transition-colors flex items-center gap-2 group"
+                  >
+                    <span className="text-[#0000EE] dark:text-[#A1A1AA] opacity-0 -ml-4 transition-all group-hover:opacity-100 group-hover:ml-0">
+                      &gt;
+                    </span>
+                    prompt-scrub
+                  </Link>
+                </li>
+                <li>
                   <a
                     href="https://github.com/Nano-Collective/json-up"
                     target="_blank"
